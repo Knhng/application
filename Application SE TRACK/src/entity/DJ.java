bd050e494c610301e15b0221f3caf466b068744d
KHOANG <Kevin.hoang@capgemini.com>
2021-09-09 13:51:17 +0200

    Class DJ created

---
+++ Application SE TRACK/src/entity/DJ.java
@@ -0,0 +1,44 @@
+package entity;
+
+public class DJ implements java.io.Serializable {
+    private Long id;
+    private String name;
+    private int age;
+    private String style;
+
+    public DJ() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
